1e6afe1054c3398e9dab22ea31af1104a6c776e8
Andrew Szymanski <madpole@gmail.com>
2017-06-10 14:54:02 +0200

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='python-generic-rest-client',
+    name='python-rest-clients',
     version='0.1',
     author='Andrew Szymanski',
     author_email='',
     packages=['cli'],
     scripts=['cli/cli.py',],
-    url='https://github.com/andrew-szymanski/python-generic-rest-client',
+    url='https://github.com/andrew-szymanski/python-rest-clients',
     license='LICENSE.txt',
-    description='Basic extendable python REST client, based on restkit',
+    description='Collection for python REST clients for PoCs, which share the same cli and pip install framework',
     long_description=open('README.md').read(),
     install_requires=[
         "simplejson>=2.5.2",
