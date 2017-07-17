fd46d9513072455eb0d9abb08b80949abad02944
Andrew Szymanski <andrew.szymanski@unibet.com>
2017-07-17 10:21:12 +0200

    separate README for each client

---
+++ helpers/README-datameer.md
@@ -0,0 +1,60 @@
+python-rest-clients
+=============
+
+Collection of python REST clients for PoCs.  The idea is that they are all in the same repo, share the same cli and each client is just an additional py module.  This is just for quick turn around PoCs.
+
+UNDER CONSTRUCTION
+
+run from source
+=============
+* checkout the code (say to path "$mycode")
+* export PYTHONPATH=$mycode
+* cd $mycode
+* cli/cli.py
+
+example:
+```shell
+export PYTHONPATH=~/data/code/github/python-rest-clients
+cd ~/data/code/github/python-rest-clients
+./cli/cli.py 
+```
+
+
+install from source
+=============
+instructions below purely as a guidelines - you may need to adjust for your case
+```shell
+git clone https://github.com/andrew-szymanski/python-rest-clients.git
+virtualenv -p /usr/bin/python python-rest-clients
+source python-rest-clients/bin/activate
+cd python-rest-clients
+pip install -e .
+# use
+# then deactivate virtualenv 
+deactivate
+
+```
+
+
+Config file
+==============
+
+
+Usage examples
+==============
+./cli/cli.py -d true -c ~/data/configs/service-discovery-client.cfg -j ~/data/configs/register.json -x helpers/service_discovery_helper.ServiceDiscoveryClient.register
+
+./cli/cli.py -d Y -x helpers/datameer_helper.DatameerClient.export_artifacts -c ./cli/datameer_client.cfg
+
+
+create egg-info
+=============
+cd $mycode
+
+python setup.py bdist_egg
+
+
+restkit 
+==============
+https://github.com/benoitc/restkit
+http://restkit.readthedocs.io/en/latest/index.html
--- helpers/datameer_helper.py
+++ helpers/datameer_helper.py
@@ -10,18 +10,41 @@ import os
 import inspect
 import time
 from array import *
+import glob
+from datetime import datetime
 
 from restkit import *
 from simplejson import load, loads, dumps
 
 # constants
 LOG_INDENT = "  "        # to prettify logs
-# constants for config keys
+# constants for config keys in cfg file / env vars
 CONFIG_URI_ROOT="DATAMEER_URI"
 CONFIG_USER_ID="DATAMEER_USER"
 CONFIG_USER_PASSWORD="DATAMEER_PASSWORD"
+CONFIG_EXPORT_FILE="DATAMEER_EXPORT_FILE"
+CONFIG_EXPORT_DIR="DATAMEER_EXPORT_DIR"
 # keys in cfg file / env variables
-CONFIG_KEYS = (CONFIG_URI_ROOT, CONFIG_USER_ID, CONFIG_USER_PASSWORD)
+CONFIG_FILE_KEYS = [CONFIG_URI_ROOT, CONFIG_USER_ID, CONFIG_USER_PASSWORD,CONFIG_EXPORT_FILE,CONFIG_EXPORT_DIR]
+
+# constants for derived config keys  
+CONFIG_APP_ROOT_DIR="APP_ROOT_DIR"
+# keys in cfg file / env variables
+CONFIG_APP_KEYS = [CONFIG_APP_ROOT_DIR]
+
+# ARTIFACTS types to export - please note that this value is used to build API url
+# so has to be exact as per API documentation (without slash)
+ARTIFACTS_TYPES = ['import-job','workbook','export-job']
+
+# path to exportable artifacts - only artifacts in this folder (or its subfolders)
+# will be exported
+EXPORTABLES_FOLDER = "/kindred-pipelines"
+
+# artifact export output filename prefix
+EXPORT_FILENAME_PREFIX = "datameer-export"
+
+
+
 
 # restkit
 # http://restkit.readthedocs.io/en/latest/api/client.html
@@ -46,7 +69,8 @@ class DatameerClient(object):
       
       # initialize variables - so all are listed here for convenience
       self.dict_config = {}    # config params
-      for config_key in CONFIG_KEYS:
+      self.artifacts_list = None    # list of datameer jobs
+      for config_key in CONFIG_FILE_KEYS:
          self.dict_config[config_key] = None
 
    def configure(self, config_file):
@@ -55,8 +79,30 @@ class DatameerClient(object):
       self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3]))         
       self.__readconfig__(config_file)
 
-   def get_jobs(self, args, kwargs):
-      """ 
+      # get root dir of this app
+      this_module_fullpath = os.path.realpath(__file__)
+      this_module_dir = os.path.dirname(this_module_fullpath)
+      this_project_dir = os.path.dirname(this_module_dir)
+      self.dict_config[CONFIG_APP_ROOT_DIR] = this_project_dir
+
+      # validate and print all derived config
+      for key in CONFIG_APP_KEYS:
+            value = self.dict_config.get(key, None)
+            if not value:
+               raise Exception("value for [%s] is NULL" % (key))
+            else:
+               self.logger.debug("%s [%s] = [%s]" % (LOG_INDENT,key, self.dict_config[key]))
+
+      # a dictionary for all exportable artifacts
+      self.all_exportable_artifacts_dict = None
+      # a dictionary for all artifacts we want to export (will contain artifacts in json string)
+      self.artifacts_to_export_json_dict = None
+      # a dictionary of artifacts we specified in artifacts_to_export.list.txt:
+      #  key = artifact name, value = 'action' (UPDATE or DELETE)
+      self.artifacts_from_list_file_dict = None
+
+   def export_artifacts(self, args, kwargs):
+      """ Main method for job export
       """
       self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3]))
       config_file = kwargs.get('cfg',"")
@@ -65,20 +111,157 @@ class DatameerClient(object):
 
       self.configure(config_file)
 
+      # read in artifacts to export - into dictionary - key is an artifact name, value is an "action"
+      self.artifacts_from_list_file_dict = self.__read_export_list__(self.dict_config[CONFIG_EXPORT_FILE])
+      if not self.artifacts_from_list_file_dict:
+         raise Exception("Could not find any artifacts to export in file: [%s]" % (self.dict_config[CONFIG_EXPORT_FILE]))
+
+      # get list of all exportable artifacts in datameer
+      self.all_exportable_artifacts_dict = self.get_artifacts()
+      count_total = 0
+      for key in self.all_exportable_artifacts_dict:
+         count_total = count_total + len(self.all_exportable_artifacts_dict[key])
+      self.logger.info("%s total exportable artifacts found: [%s]..." % (LOG_INDENT, count_total))
+
+
+      # get articacts to export - again, dict, key is artifact type, value is a list of artifacts for that type
+      self.logger.info("%s selecting artifacts specified in [%s] file..." % (LOG_INDENT, self.dict_config[CONFIG_EXPORT_FILE]))
+      self.artifacts_to_export_json_dict = self.__get_artifacts_to_export__(self.artifacts_from_list_file_dict)
+
+      # write out artifacts in json so we can import them
+      self.__write_out_artifacts__()
+
+
+   def get_artifacts(self):
+      """ Get a list of all artifacts, i.e  import jobs, export jobs and workbooks,
+      but only if they are in EXPORTABLES_FOLDER or its subfolders
+      """ 
+      self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3]))
+
       # root url
       uri_root = self.dict_config[CONFIG_URI_ROOT]
       # authentication
       auth = BasicAuth(self.dict_config[CONFIG_USER_ID], self.dict_config[CONFIG_USER_PASSWORD])
-      # list all jobs
-      uri = uri_root + "/export-job"
-      resource = Resource(uri,filters=[auth])
-      self.logger.debug("getting list of all jobs...") 
-      #print dumps(payload)
-      timestamp = time.strftime("%m-%m-%d %H:%M")
-      response = resource.get(headers={'Content-Type': 'application/json'})
-      result_str = response.body_string()
-      #self.logger.debug("%s response: %s" %  (LOG_INDENT, response.__dict__) )      
-      self.logger.debug("%s response: %s" %  (LOG_INDENT, result_str) )      
+
+      # dictionary - each key is url for artifact type - and value will be a list of all artifacts for that type
+      ret_dict = {}
+      ignored_count = 0           # count all artifacts that matched but were in different folder
+
+      for artifact_url in ARTIFACTS_TYPES:
+         uri = "%s/%s" % (uri_root,artifact_url)
+         resource = Resource(uri,filters=[auth])
+         self.logger.debug("%s getting artifacts of [%s]..." % (LOG_INDENT, artifact_url)) 
+         timestamp = time.strftime("%m-%m-%d %H:%M")
+         response = resource.get(headers={'Content-Type': 'application/json'})
+         result_str = response.body_string()
+         result_list = loads(result_str)
+         clean_list = []
+         
+         # exclude all artifacts which are not in EXPORTABLES_FOLDER
+         for artifact in result_list:
+            artifact_path = artifact['path']
+            #print "[%s] [%s] [%s]" % (artifact_path, EXPORTABLES_FOLDER, artifact_path.startswith(EXPORTABLES_FOLDER))
+            if artifact_path.startswith(EXPORTABLES_FOLDER) == False:
+               ignored_count = ignored_count + 1
+               continue
+            clean_list.append(artifact)
+         
+         ret_dict[artifact_url] = clean_list
+         self.logger.debug("%s %s [%s] --> [%s] artifacts loaded" % (LOG_INDENT, LOG_INDENT, artifact_url, len(ret_dict[artifact_url]))) 
+
+      if ignored_count > 0:
+         self.logger.warn("[%s] artifacts were ignored because they weren't in [%s] folder" % (ignored_count, EXPORTABLES_FOLDER))
+ 
+      return ret_dict
+
+   def __get_artifacts_to_export__(self, artifacts_name_dict):
+      """ extract artifacts we actually want to export
+      artifacts_name_list is dictionary, key is an artifact name, value is an "action"
+      """ 
+      self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3]))
+
+      result_dict = {}
+
+      # iterate through all exportable artifacts - trying to match them (by name) to artifacts specified for export
+      for artifact_type in self.all_exportable_artifacts_dict:
+         all_artifacts_for_type_list = self.all_exportable_artifacts_dict[artifact_type]
+         self.logger.debug("%s ------  artifact type: [%s], total: [%s]  ------" % (LOG_INDENT, artifact_type, len(all_artifacts_for_type_list)))
+         # initialise output list
+         output_json_list = []
+         # all_artifacts_for_type_list is a list of dictionaries, we want to match on entry with key 'name'
+         for item_dict in all_artifacts_for_type_list:
+            # check if it is on our list of artifacts to export
+           if item_dict['name'] in artifacts_name_dict:
+               # we found artifact to export - we now need to get full json for it
+               uri = "%s/%s/%s" % (self.dict_config[CONFIG_URI_ROOT], artifact_type,item_dict['id'])
+               auth = BasicAuth(self.dict_config[CONFIG_USER_ID], self.dict_config[CONFIG_USER_PASSWORD])
+               resource = Resource(uri,filters=[auth])
+               self.logger.debug("%s getting artifact [%s], [%s]..." % (LOG_INDENT, item_dict['name'], uri)) 
+               response = resource.get(headers={'Content-Type': 'application/json'})
+               result_json = response.body_string()
+               # print result_json
+               # we now have json to import to another env... store it 
+               output_json_list.append(result_json)
+         # we finished iterating through all artifacts of that type - so add list to our return dictionary
+         result_dict[artifact_type] = output_json_list
+
+      # finished all loops
+      return result_dict
+
+
+
+   def __write_out_artifacts__(self):
+      """ write out artifacts to export to a file
+      """
+      self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3]))
+
+      # create dir if it does not exist
+      self.logger.debug("%s making sure dir exists: [%s]..." % (LOG_INDENT, self.dict_config[CONFIG_EXPORT_DIR]))
+      if not os.path.exists(self.dict_config[CONFIG_EXPORT_DIR]):
+         try:
+            os.makedirs(self.dict_config[CONFIG_EXPORT_DIR])
+         except Exception, e:
+            raise Exception("Could not create dir: [%s], error: [%s]" % (self.dict_config[CONFIG_EXPORT_DIR], e))
+
+      # delete all files by extension - noddy but safer than using * wildcard or deleting whole dir
+      files_path = "%s/*.json" % self.dict_config[CONFIG_EXPORT_DIR]
+      self.logger.debug("%s deleting all files: [%s]..." % (LOG_INDENT, files_path))
+      files = glob.glob(files_path)
+      for f in files:
+         os.remove(f)
+      
+
+      # self.artifacts_from_list_file_dict
+
+      # OK - now iterate through all artifacts to export and write a separate json file for each
+      self.logger.debug("%s starting to write out json files..." % (LOG_INDENT))
+      for artifact_type in self.artifacts_to_export_json_dict:
+         for artifact_json in self.artifacts_to_export_json_dict[artifact_type]:
+            # noddy - load json so we can determine some properties... you may ask why not load json to start with 
+            # and write it out here, rather than doing both?  Well - this is extra safety net - so we write json EXACTLY
+            # how we got it from datameer
+            artifact_dict = {}
+            artifact_dict = loads(artifact_json)
+            artifact_name = artifact_dict['file']['name']
+            artifact_uuid = artifact_dict['file']['uuid']
+            artifact_action = self.artifacts_from_list_file_dict[artifact_name]
+            artifact_action = artifact_action.upper()
+
+            # and now write out
+            now = datetime.now()
+            timestamp = now.strftime("%y-%m-%d.%H%M.%f")
+            output_filename = "%s/%s.%s.%s.%s.%s.%s.json" % (
+               self.dict_config[CONFIG_EXPORT_DIR],
+               EXPORT_FILENAME_PREFIX,
+               artifact_type,
+               artifact_action,
+               artifact_name,
+               artifact_uuid,
+               timestamp)
+            with open(output_filename, "w") as text_file:
+               text_file.write(artifact_json)
+            self.logger.debug("%s %s [%s] done" % (LOG_INDENT,LOG_INDENT,output_filename))
+ 
 
 
    def __readconfig__(self, config_file):
@@ -107,7 +290,7 @@ class DatameerClient(object):
             raise Exception("Could not read config file: [%s], error: [%s]" % (config_file, e))
 
       # and check if env vars with the same name exist - and overwrite values from file if they do
-      for key in CONFIG_KEYS:
+      for key in CONFIG_FILE_KEYS:
          value = os.getenv(key, None)
          if value:
             self.logger.debug("%s env var [%s] found so it will be used instead, overridng value in conf file" % (LOG_INDENT, key))
@@ -118,13 +301,15 @@ class DatameerClient(object):
       self.dict_config[CONFIG_USER_PASSWORD] = password
 
       # validate all params
-      for key in CONFIG_KEYS:
+      for key in CONFIG_FILE_KEYS:
             value = self.dict_config.get(key, None)
             if not value:
                raise Exception("mandatory value for [%s] not defined in config file [%s] or as env variable. " % (key, config_file))
 
       self.logger.info("%s %s: [%s]" % (LOG_INDENT, CONFIG_URI_ROOT, self.dict_config[CONFIG_URI_ROOT]))
       self.logger.info("%s %s: [%s]" % (LOG_INDENT, CONFIG_USER_ID, self.dict_config[CONFIG_USER_ID]))               
+      self.logger.info("%s %s: [%s]" % (LOG_INDENT, CONFIG_EXPORT_FILE, self.dict_config[CONFIG_EXPORT_FILE]))               
+      self.logger.info("%s %s: [%s]" % (LOG_INDENT, CONFIG_EXPORT_DIR, self.dict_config[CONFIG_EXPORT_DIR]))               
   
    def __get_password__(self):     
       self.logger.debug("%s %s::%s starting..." %  (LOG_INDENT,self.__class__.__name__ , inspect.stack()[0][3]))
@@ -146,7 +331,29 @@ class DatameerClient(object):
       password = password_value
       return password
 
+   def __read_export_list__(self, job_list_file):
+      """ read in file specifying artifacts to export
+      """
+      self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3]))         
+      artifacts_to_export_dict = {}
 
+      try:
+         with open(job_list_file) as f:
+            for line in f:
+               line = line.strip()
+               if line.startswith("#"):           # comment line
+                  continue
+               if not line:                       # empty line
+                  continue
+               # split into tuple - 1st element is "action", 2nd is a job name
+               (action, artifact_name) = line.split(":",1)
+               artifacts_to_export_dict[artifact_name] = action
+ 
+      except Exception, e:
+         raise Exception("Could not read jobs to export file: [%s], error: [%s]" % (job_list_file, e))
 
+      self.logger.info("%s number of jobs to export: [%s]" % (LOG_INDENT, len(artifacts_to_export_dict)))
+      return artifacts_to_export_dict
       
+
 
